8721e1d820036021970bb89e9308eb90728bcb30
Bek <kruzus@live.com>
2021-05-18 19:30:02 -0400

    needs work

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navbox">
     <h3 class="navlogo">Logo</h3>
     <div class="navitems">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
   </nav>
@@ -23,5 +23,16 @@ export default defineComponent({
   border: 1px solid black;
   height: 80px;
   display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  .navitems {
+    border: 1px solid pink;
+    font-size: 1.2em;
+    padding: 10px;
+  }
+}
+.navlogo {
+  font-size: 1em;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-<h1>Hello</h1>
+  <main>
+    <H1>this is header</H1>
+    <div>
+      Sometimes people just say the meanest things lol awdawdawd Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Libero illo quasi provident,
+      obcaecati magni ipsa eligendi deserunt qui culpa earum consectetur
+      doloribus praesentium et voluptas possimus facilis dolores labore
+      veritatis?
+    </div>
+  </main>
 </template>
-
- 
