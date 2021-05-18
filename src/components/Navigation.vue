ce91802ff1c709da699a71bde922b96bfa29b61a
Bek <kruzus@live.com>
2021-05-18 19:07:24 -0400

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbox">
+    <h3 class="navlogo">Logo</h3>
+    <div class="navitems">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Navigation",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.navbox {
+  border: 1px solid black;
+  height: 80px;
+  display: flex;
+}
+</style>
